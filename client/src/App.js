4b9feabaf26d402666c6d9334343c3898f6f00c2
starhms <starhms@gmail.com>
2021-05-14 15:40:50 +0900

    update ui/ux add

--- client/src/App.js
+++ client/src/App.js
@@ -12,17 +12,88 @@ import CirularProgress from '@material-ui/core/CircularProgress';
 import { withStyles} from '@material-ui/core/styles';
 
 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import { fade } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+
+
 const styles = theme => ({
   root: {
     width: '100%',
-    marginTop: theme.spacing(1),
-    overflowX: "auto"
-  },
-  table: {
     minWidth: 1080
+  },
+  menu: {
+    marginTop: 15,
+    marginBottom: 15,
+    display : 'flex',
+    justifyContent: 'center'
+  },
+  paper: {
+    marginLeft : 18,
+    marginRight: 18
   },
   progress: {
     margin: theme.spacing(2)
+  },
+  grow: {
+    flexGrow:1, 
+  },
+  tableHead: {
+    fontSize: '1.0rem'
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
   }
 })
 
@@ -34,18 +105,22 @@ class App extends Component {
     super(props);
     this.state = {
       customers: '' ,
-      completed:0
+      completed:0,
+      searchKeyword: '',
     }
     this.stateRefresh = this.stateRefresh.bind(this);
+    this.handleValueChange = this.handleValueChange.bind(this);
   }
 
 
   stateRefresh() {
     this.setState({
     customers: '',
-    completed: 0
+    completed: 0,
+    searchKeyword: ''
     });
-  this.callApi()
+  
+    this.callApi()
     .then(res => this.setState({customers: res}))
     .catch(err => console.log(err));
 }
@@ -75,45 +150,82 @@ class App extends Component {
    this.setState ({ completed: completed >= 100 ? 0 : completed + 1});
   }
 
+  handleValueChange = (e) => {
+    let nextState = {};
+    nextState[e.target.name] = e.target.value;
+    this.setState(nextState);
+  }
+
   render(){
+    const filteredComponents = (data) => {
+      data = data.filter((c) => {
+        return c.device_name.indexOf(this.state.searchKeyword) > -1;
+      });
+      return data.map((c) => {
+        return <Customer stateRefresh={this.stateRefresh} key={c.device_name}
+        id={c.id}
+        device_name={c.device_name}
+        user_id={c.user_id}
+        password={c.password}
+        password2={c.password2}
+        ip_address={c.ip_address}
+        policy_id={c.policy_id}
+        cisco_group={c.cisco_group}
+        product={c.product}
+        createDate={c.createDate}
+        />
+      });
+    }
   const { classes } = this.props;
+  const cellList = [ "순번", "장비명", "제품군", "접속계정","패스워드","IP ADDRESS", "정책명(번호)", "패스워드(Cisco 전용)", "그룹명(Cisco 전용)", "등록일시", "삭제" ]; 
   return (
-    <div>
-        <Paper className={classes.root}>
+    <div className={classes.root}>
+    <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="open drawer"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
+            장비 관리 시스템
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="검색하기"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              name="searchKeyword"
+              value={this.state.searchKeyword}
+              onChange={this.handleValueChange}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.menu}>
+      <CustomerAdd stateRefresh={this.stateRefresh}/>
+      </div>
+        <Paper className={classes.paper}>
           <Table className={classes.table}>
+            
               <TableHead>
                 <TableRow>
-                  <TableCell>순번</TableCell>
-                  <TableCell>장비명</TableCell>
-                  <TableCell>제품군</TableCell>
-                  <TableCell>접속계정</TableCell>
-                  <TableCell>패스워드</TableCell>
-                  <TableCell>IP ADDRESS</TableCell>
-                  <TableCell>정책명(번호)</TableCell>
-                  <TableCell>패스워드(Cisco 전용)</TableCell>
-                  <TableCell>그룹명(Cisco 전용)</TableCell>
-                  <TableCell>등록일시</TableCell>
-                  <TableCell>설정</TableCell>
+                  {cellList.map(c => {
+                    return <TableCell className={classes.tableHead}>{c}</TableCell>
+                  })}
                 </TableRow>
               </TableHead>
             <TableBody>
-                {this.state.customers ? this.state.customers.map(c => {
-                  return(
-                    <Customer stateRefresh={this.stateRefresh}
-                    key={c.device_name}
-                    id={c.id}
-                    device_name={c.device_name}
-                    user_id={c.user_id}
-                    password={c.password}
-                    password2={c.password2}
-                    ip_address={c.ip_address}
-                    policy_id={c.policy_id}
-                    cisco_group={c.cisco_group}
-                    product={c.product}
-                    createDate={c.createDate}
-                    />
-                  )
-              }) :
+                {this.state.customers ? 
+                  filteredComponents(this.state.customers) : 
               <TableRow>
                 <TableCell colSpan="6" align="center">
                   <CirularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
@@ -123,7 +235,7 @@ class App extends Component {
             </TableBody>
           </Table>
         </Paper>
-        <CustomerAdd stateRefresh={this.stateRefresh}/>
+        
     </div>
   )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
-ReactDOM.render(
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: '"Noto Sans KR", serif',
+  }
+})
+
+ReactDOM.render( <MuiThemeProvider theme={theme}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode></MuiThemeProvider>,
   document.getElementById('root')
 );
 
